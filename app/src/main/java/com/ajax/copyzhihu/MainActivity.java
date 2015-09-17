a404f01563fef6a8ee50375aeda7e4f90af444d4
ajaxshang <ajaxshang@163.com>
2015-09-17 16:22:37 +0800

    添加一些新的控件
        NavigationView
        CoordinatorLayout
        FloatingActionButton
        RecyclerView
        AppBarLayout
        CollapsingToolbarLayout
        Toolbar

--- app/src/main/java/com/ajax/copyzhihu/MainActivity.java
+++ app/src/main/java/com/ajax/copyzhihu/MainActivity.java
@@ -1,18 +1,18 @@
 package com.ajax.copyzhihu;
 
-import android.app.Activity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.view.Menu;
 import android.view.MenuItem;
 
-public class MainActivity extends Activity {
+public class MainActivity extends AppCompatActivity {
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.main);
     }
-
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
---
+++ app/src/main/java/com/ajax/copyzhihu/fragment/MenuFragment.java
@@ -0,0 +1,7 @@
+package com.ajax.copyzhihu.fragment;
+
+/**
+ * Created by Administrator on 2015/9/17.
+ */
+public class MenuFragment {
+}
